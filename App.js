1de53597d555c319edefe01797c958ad1eca4aad
giovinnakho <giovinnakhoharja@yahoo.com>
2019-03-09 00:53:23 +0700

    button and input

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert } from 'react-native';
+import { stringify } from 'qs';
 
 class Greeting extends React.Component {
   constructor(props) {
@@ -27,17 +28,48 @@ class Greeting extends React.Component {
 }
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ' '}
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({text: event.replace(/ /g, '¢')});
+  }
+
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
     }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Image source={pic} style={{width: 193, height: 110}} />
-        <Greeting person_name='Maria' />
-        <Greeting person_name='Bella' />
-        <Greeting person_name='Orang' />
+      //# object class & styles
+      // <View style={styles.container}>
+      //   <Text style={styles.bigBlue}>Open up App.js to start working on your app!</Text>
+      //   <Text style={styles.red}>This one is red</Text>
+      //   <Image source={pic} style={{width: 193, height: 110}} />
+      //   <Greeting person_name="Maria" />
+      //   <Greeting person_name="Bella" />
+      //   <Greeting person_name="Orang" />
+      // </View>
+
+      //# layout
+      // <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'flex-end'}}>
+      //   <View style={{width: 50, height: 50, backgroundColor:'powderblue'}}></View>
+      //   <View style={{width: 50, height: 50, backgroundColor:'skyblue'}}></View>
+      //   <View style={{width: 50, height: 50, backgroundColor:'steelblue'}}></View>
+      // </View>
+
+      //# textinput
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        {/* <TextInput style={{flex: 1, height: 40}} placeholder='Type here!' onChangeText={(text) => this.setState({text})}/> */}
+        {/* <Text style={{flex: 1, padding: 10, fontSize: 42}}>{this.state.text.split(' ').map((word) => word && '£').join(' ')}</Text> */}
+        <TextInput style={{flex: 1, height: 40}} placeholder='Type here!' onChangeText={this.handleChange} />
+        <Text style={{flex: 1, padding: 1}}>{this.state.text}</Text>
+        <Button style={{flex: 1}} onPress={() => {
+          Alert.alert('You pressed the button');
+        }} title="Press me"><Text>Press</Text></Button>
       </View>
     );
   }
@@ -49,5 +81,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
